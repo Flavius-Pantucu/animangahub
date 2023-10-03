7de45fe6ba8f8697cf3cd02a1cdcd047acd69325
Flavius Pantucu <flaviuspantucu@icloud.com>
2023-10-03 11:50:44 +0300

    swapped remix js for next js

--- backend/app.js
+++ backend/app.js
@@ -4,9 +4,9 @@ require("dotenv").config();
 
 const NODE_ENV = process.argv[2];
 if (NODE_ENV === undefined)
-  return console.log("Expected node environment argument to be specified");
+  return res.send("Expected node environment argument to be specified");
 else if (NODE_ENV !== "PROD" && NODE_ENV !== "DEV")
-  return console.log("Unexpected value for node environment argument");
+  return res.send("Unexpected value for node environment argument");
 
 // JWT Configuration
 const jwt = require("jsonwebtoken");
@@ -20,7 +20,15 @@ app.listen(port, () => console.log(`Listening on port ${port}!`));
 //MongoBD Configuration
 const uri =
   NODE_ENV == "PROD" ? process.env.PROD_MONGO_URI : process.env.DEV_MONGO_URI;
-const client = new MongoClient(uri, { useUnifiedTopology: true });
+const client = new MongoClient(
+  uri,
+  { useUnifiedTopology: true },
+  { useNewUrlParser: true },
+  { connectTimeoutMS: 30000 },
+  { keepAlive: 1 }
+);
+
+client.connect();
 
 // Routes
 app.get("/", function (req, res) {
@@ -28,173 +36,130 @@ app.get("/", function (req, res) {
   res.send("Welocme to AniMangaHub!");
 });
 
-app.get("/test", function (req, res, next) {
-  console.log("GET request called");
-  res.send("GET request called");
+app.get("/get-recipes", async (req, res, next) => {
+  const dbName =
+    NODE_ENV == "PROD"
+      ? process.env.PROD_MONGO_DATABASE
+      : process.env.DEV_MONGO_DATABASE;
+
+  const collectionName = "recipes";
+
+  const database = client.db(dbName);
+  const collection = database.collection(collectionName);
+
+  try {
+    const cursor = await collection.find().toArray();
+
+    res.send(cursor);
+  } catch (err) {
+    res.send(`Something went wrong trying to find the documents: ${err}\n`);
+  }
 });
 
-app.post("/test", function (req, res, next) {
-  console.log("POST request called");
-  res.send("POST request called");
+app.get("/get-recipe", async (req, res, next) => {
+  const dbName =
+    NODE_ENV == "PROD"
+      ? process.env.PROD_MONGO_DATABASE
+      : process.env.DEV_MONGO_DATABASE;
+
+  const collectionName = "recipes";
+
+  const database = client.db(dbName);
+  const collection = database.collection(collectionName);
+
+  const findOneQuery = { ingredients: req.query.ingredient };
+
+  try {
+    const findOneResult = await collection.findOne(findOneQuery);
+
+    if (findOneResult === null) {
+      res.send(
+        "Couldn't find any recipes that contain " +
+          req.query.ingredient +
+          " as an ingredient.\n"
+      );
+    } else {
+      res.send(findOneResult);
+    }
+  } catch (err) {
+    res.send(`Something went wrong trying to find one document: ${err}\n`);
+  }
 });
 
-app.delete("/test", function (req, res, next) {
-  console.log("DELETE request called");
-  res.send("DELETE request called");
+app.post("/add-recipe", async (req, res, next) => {
+  const dbName =
+    NODE_ENV == "PROD"
+      ? process.env.PROD_MONGO_DATABASE
+      : process.env.DEV_MONGO_DATABASE;
+
+  const collectionName = "recipes";
+
+  const database = client.db(dbName);
+  const collection = database.collection(collectionName);
+
+  const recipe = {
+    name: "cozonac",
+    ingredients: ["faina", "oua", "lapte", "drojdie", "esente de rom"],
+    prepTimeInMinutes: 120,
+  };
+
+  try {
+    const insertOne = await collection.insertOne(recipe);
+    res.send(`${insertOne.insertedCount} documents successfully inserted.\n`);
+  } catch (err) {
+    res.send(
+      `Something went wrong trying to insert the new documents: ${err}\n`
+    );
+  }
 });
 
-app.put("/test", function (req, res, next) {
-  console.log("PUT request called");
-  res.send("PUT request called");
+app.put("/update-recipe", async (req, res, next) => {
+  const dbName =
+    NODE_ENV == "PROD"
+      ? process.env.PROD_MONGO_DATABASE
+      : process.env.DEV_MONGO_DATABASE;
+
+  const collectionName = "recipes";
+
+  const database = client.db(dbName);
+  const collection = database.collection(collectionName);
+
+  const findOneQuery = { name: "cozonac" };
+
+  const updateDoc = { $set: { prepTimeInMinutes: 180 } };
+
+  const updateOptions = { returnOriginal: false };
+
+  try {
+    const updateResult = await collection.findOneAndUpdate(
+      findOneQuery,
+      updateDoc,
+      updateOptions
+    );
+    res.send(
+      `Here is the updated document:\n${JSON.stringify(updateResult.value)}\n`
+    );
+  } catch (err) {
+    res.send(`Something went wrong trying to update one document: ${err}\n`);
+  }
 });
 
-// mongodb examples
+app.delete("/delete-recipe", async (req, res, next) => {
+  const dbName =
+    NODE_ENV == "PROD"
+      ? process.env.PROD_MONGO_DATABASE
+      : process.env.DEV_MONGO_DATABASE;
 
-// await client.connect();
+  const collectionName = "recipes";
 
-// const dbName = process.env.MONGO_DATABASE;
-// const collectionName = "recipes";
+  const database = client.db(dbName);
+  const collection = database.collection(collectionName);
 
-// const database = client.db(dbName);
-// const collection = database.collection(collectionName);
-
-// const recipes = [
-//   {
-//     name: "elotes",
-//     ingredients: [
-//       "corn",
-//       "mayonnaise",
-//       "cotija cheese",
-//       "sour cream",
-//       "lime",
-//     ],
-//     prepTimeInMinutes: 35,
-//   },
-//   {
-//     name: "loco moco",
-//     ingredients: [
-//       "ground beef",
-//       "butter",
-//       "onion",
-//       "egg",
-//       "bread bun",
-//       "mushrooms",
-//     ],
-//     prepTimeInMinutes: 54,
-//   },
-//   {
-//     name: "patatas bravas",
-//     ingredients: [
-//       "potato",
-//       "tomato",
-//       "olive oil",
-//       "onion",
-//       "garlic",
-//       "paprika",
-//     ],
-//     prepTimeInMinutes: 80,
-//   },
-//   {
-//     name: "fried rice",
-//     ingredients: [
-//       "rice",
-//       "soy sauce",
-//       "egg",
-//       "onion",
-//       "pea",
-//       "carrot",
-//       "sesame oil",
-//     ],
-//     prepTimeInMinutes: 40,
-//   },
-// ];
-
-// try {
-//   const insertManyResult = await collection.insertMany(recipes);
-//   console.log(
-//     `${insertManyResult.insertedCount} documents successfully inserted.\n`
-//   );
-// } catch (err) {
-//   console.error(
-//     `Something went wrong trying to insert the new documents: ${err}\n`
-//   );
-// }
-
-// /*
-//  * *** FIND DOCUMENTS ***
-//  */
-
-// const findQuery = { prepTimeInMinutes: { $lt: 45 } };
-
-// try {
-//   const cursor = await collection.find(findQuery).sort({ name: 1 });
-//   await cursor.forEach((recipe) => {
-//     console.log(
-//       `${recipe.name} has ${recipe.ingredients.length} ingredients and takes ${recipe.prepTimeInMinutes} minutes to make.`
-//     );
-//   });
-//   // add a linebreak
-//   console.log();
-// } catch (err) {
-//   console.error(
-//     `Something went wrong trying to find the documents: ${err}\n`
-//   );
-// }
-
-// // We can also find a single document. Let's find the first document
-// // that has the string "potato" in the ingredients list.
-// const findOneQuery = { ingredients: "potato" };
-
-// try {
-//   const findOneResult = await collection.findOne(findOneQuery);
-//   if (findOneResult === null) {
-//     console.log(
-//       "Couldn't find any recipes that contain 'potato' as an ingredient.\n"
-//     );
-//   } else {
-//     console.log(
-//       `Found a recipe with 'potato' as an ingredient:\n${JSON.stringify(
-//         findOneResult
-//       )}\n`
-//     );
-//   }
-// } catch (err) {
-//   console.error(`Something went wrong trying to find one document: ${err}\n`);
-// }
-
-// /*
-//  * *** UPDATE A DOCUMENT ***
-//  */
-// const updateDoc = { $set: { prepTimeInMinutes: 72 } };
-
-// const updateOptions = { returnOriginal: false };
-
-// try {
-//   const updateResult = await collection.findOneAndUpdate(
-//     findOneQuery,
-//     updateDoc,
-//     updateOptions
-//   );
-//   console.log(
-//     `Here is the updated document:\n${JSON.stringify(updateResult.value)}\n`
-//   );
-// } catch (err) {
-//   console.error(
-//     `Something went wrong trying to update one document: ${err}\n`
-//   );
-// }
-
-// /*
-//  * *** DELETE DOCUMENTS ***
-//  */
-
-// const deleteQuery = { name: { $in: ["elotes", "fried rice"] } };
-// try {
-//   const deleteResult = await collection.deleteMany(deleteQuery);
-//   console.log(`Deleted ${deleteResult.deletedCount} documents\n`);
-// } catch (err) {
-//   console.error(`Something went wrong trying to delete documents: ${err}\n`);
-// }
-
-// // Make sure to call close() on your client to perform cleanup operations
-// await client.close();
+  const deleteQuery = { name: "cozonac" };
+  try {
+    const deleteResult = await collection.deleteOne(deleteQuery);
+    res.send(`Deleted ${deleteResult.deletedCount} documents\n`);
+  } catch (err) {
+    res.send(`Something went wrong trying to delete documents: ${err}\n`);
+  }
+});
